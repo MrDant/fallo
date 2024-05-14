f22af8c1514e6cb3be58db41783dbaf3ad775877
MrDant <mael.dantuono@gmail.com>
2024-05-14 23:33:44 +0200

    Component : Header

---
+++ nuxt-ui-app/components/header.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex justify-center items-center">
+    <div class="rounded-full overflow-hidden w-24">
+      <NuxtImg src="/logo.jpeg" />
+    </div>
+    <div class="ml-11 pt-10">
+      <p class="text-tangaroa pacifico-regular">Fallo !</p>
+      <p class="text-maroon-flush pacifico-regular pl-5">Non pensare</p>
+    </div>
+  </div>
+</template>
--- nuxt-ui-app/layouts/default.vue
+++ nuxt-ui-app/layouts/default.vue
@@ -1,3 +1,22 @@
 <template>
-  <div><slot /></div>
+  <div class="my-container">
+    <UCard class="card">
+      <template #header>
+        <Header />
+      </template>
+
+      <div><slot /></div>
+    </UCard>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+.my-container {
+  @apply h-1 min-h-screen;
+  @apply md:flex items-center justify-center;
+}
+.card {
+  @apply h-full shadow;
+  @apply md:h-auto md:min-w-96;
+}
+</style>
