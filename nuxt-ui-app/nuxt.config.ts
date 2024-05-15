a2d08c15f89c05f0bd6e90581381ec931bff022a
MrDant <mael.dantuono@gmail.com>
2024-05-15 20:44:55 +0200

    System: PWA

--- nuxt-ui-app/nuxt.config.ts
+++ nuxt-ui-app/nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxt/ui", "@nuxt/image"],
+  modules: ["@nuxt/ui", "@nuxt/image", "@vite-pwa/nuxt"],
   css: ["~/assets/css/app.scss"],
+  pwa: {
+    manifest: { theme_color: "#021943" },
+  },
 });
