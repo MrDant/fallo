0c82df4e50f8bfb45870833a26f5736042ec7134
MrDant <mael.dantuono@gmail.com>
2024-05-18 16:02:11 +0200

    feature(header) add home link

--- nuxt-ui-app/components/header.vue
+++ nuxt-ui-app/components/header.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="flex justify-center items-center">
-    <div class="rounded-full overflow-hidden w-24">
+    <NuxtLink to="/" class="rounded-full overflow-hidden w-24">
       <NuxtImg src="/logo.jpeg" />
-    </div>
+    </NuxtLink>
     <div class="ml-11 pt-10">
       <p class="text-tangaroa pacifico-regular">Fallo !</p>
       <p class="text-maroon-flush pacifico-regular pl-5">Non pensare</p>
