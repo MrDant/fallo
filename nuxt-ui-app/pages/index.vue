4a1f1611461cee90602019c2992198e1a381353a
MrDant <mael.dantuono@gmail.com>
2024-05-18 16:02:40 +0200

    feat(session list) update design + remove button

--- nuxt-ui-app/pages/index.vue
+++ nuxt-ui-app/pages/index.vue
@@ -3,29 +3,60 @@
     <div class="text-end">
       <NuxtLink to="/create">+ Nouveau</NuxtLink>
     </div>
-    <div class="flex flex-col gap-2">
-      <div v-for="session in data" :key="session" class="session-card">
+    <Loading v-if="loading" />
+    <div class="flex flex-col gap-2" v-else>
+      <div v-for="session in data" :key="session.id" class="session-card">
         <div class="session-col">
           <p class="text-tangaroa pacifico-regular">{{ session.title }}</p>
           <p class="pl-2 pb-2">{{ session.actions.length }} exercice(s)</p>
         </div>
-        <div class="rounded-full overflow-hidden w-32 h-32">
+        <NuxtLink
+          :to="'/' + session.id"
+          class="rounded-full overflow-hidden w-32 h-32"
+        >
           <NuxtImg :src="session.img" class="w-full h-full" />
-        </div>
-        <div class="session-col text-2xl justify-self-end">
-          <NuxtLink :to="'/edit/' + session.id">
+        </NuxtLink>
+        <div class="session-col justify-self-end">
+          <UButton
+            @click="deleteSession(session.id)"
+            variant="link"
+            class="text-maroon-flush text-2xl"
+          >
+            <UIcon name="i-heroicons-x-circle-16-solid" />
+          </UButton>
+          <UButton
+            variant="link"
+            @click="navigateTo('/edit/' + session.id)"
+            class="text-kabul text-2xl"
+          >
             <UIcon name="i-heroicons-paint-brush-solid" />
-          </NuxtLink>
-          <NuxtLink :to="'/' + session.id">
-            <UIcon name="i-heroicons-play-circle-solid" />
-          </NuxtLink>
+          </UButton>
         </div>
       </div>
     </div>
   </div>
 </template>
 <script setup>
-const { data } = useDBStore();
+const { removeSession } = useDBStore();
+const { data } = storeToRefs(useDBStore());
+const loading = ref(false);
+
+function deleteSession(id) {
+  useToast().add({
+    title: "Suppression de la session",
+    description: "Êtes-vous sur de vous supprimer cette session ?",
+    actions: [
+      {
+        label: "Confirmer",
+        click: async () => {
+          loading.value = true;
+          await removeSession(id);
+          loading.value = false;
+        },
+      },
+    ],
+  });
+}
 </script>
 <style lang="scss" scoped>
 .session-card {
