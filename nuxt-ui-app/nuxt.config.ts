611b1a4a0727a9efbac8d5bbcd81ef67c56c73d2
MrDant <mael.dantuono@gmail.com>
2024-05-19 15:11:23 +0200

    System(theme): remove dark mode

--- nuxt-ui-app/nuxt.config.ts
+++ nuxt-ui-app/nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  colorMode: {
+    preference: "light",
+  },
   modules: ["@nuxt/ui", "@nuxt/image", "@pinia/nuxt"],
   css: ["~/assets/css/app.scss"],
   ui: {
